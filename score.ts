984a9138a008152e48172d89bb85a3b53f84aac1
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-07-04 04:01:20 +0900

    fix

--- score.ts
+++ score.ts
@@ -6,27 +6,31 @@ import {
 import { lookup, REGISTRIES } from "https://deno.land/x/udd@0.7.3/registry.ts";
 import * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
 
-export async function getDependenciesScores(rootSpecifier: string) {
-  const res: {
+export interface DependenciesScoreResult {
+  score: number;
+  data: {
     specifier: string;
     importFrom: string[];
     score: number;
     latestVersion: string | null;
     message: string;
-  }[] = [];
+  }[];
+}
+
+export async function getDependenciesScores(
+  rootSpecifier: string,
+): Promise<DependenciesScoreResult> {
+  const res: DependenciesScoreResult["data"] = [];
   const graph = await createGraph(rootSpecifier);
   const moduleTree = getExported(graph);
   for (const module of graph.modules) {
     if (module.specifier === rootSpecifier) {
       continue;
     }
-    const { score, latestVersion, message } = await getScore(module);
     res.push({
       specifier: module.specifier,
       importFrom: moduleTree[module.specifier] ?? [],
-      score,
-      latestVersion,
-      message,
+      ...await getScore(module),
     });
   }
   return {
@@ -35,12 +39,20 @@ export async function getDependenciesScores(rootSpecifier: string) {
   };
 }
 
-async function getScore(
-  module: Module,
-): Promise<{ score: number; latestVersion: string | null; message: string }> {
+async function getScore(module: Module): Promise<{
+  score: number;
+  currentVersion: string | null;
+  latestVersion: string | null;
+  message: string;
+}> {
   const registry = lookup(module.specifier, REGISTRIES);
   if (!registry) {
-    return { score: 0, latestVersion: null, message: "Registry not found" };
+    return {
+      score: 0,
+      currentVersion: null,
+      latestVersion: null,
+      message: "Registry not found",
+    };
   }
 
   let currentVersion: string;
@@ -50,49 +62,69 @@ async function getScore(
     console.error(e);
     return {
       score: 0,
+      currentVersion: null,
       latestVersion: null,
       message: "Failed to parse the current version",
     };
   }
   const latestVersion = (await registry.all())[0];
   if (currentVersion === latestVersion) {
-    return { score: 1, latestVersion, message: "Latest version is used" };
+    return {
+      score: 1,
+      currentVersion,
+      latestVersion,
+      message: "Latest version is used",
+    };
   }
 
   const currentSemver = semver.parse(currentVersion);
   const latestSemver = semver.parse(latestVersion);
   if (!currentSemver) { // not pinned
-    return { score: 0, latestVersion, message: "Version is not pinned" };
+    return {
+      score: 0,
+      currentVersion,
+      latestVersion,
+      message: "Version is not pinned",
+    };
   }
   if (!latestSemver) {
     return {
       score: 0.2,
-      latestVersion: null,
+      currentVersion,
+      latestVersion,
       message: "Failed to parse the latest version",
     };
   }
   if (currentSemver.major !== latestSemver.major) {
     return {
       score: 0.3,
-      latestVersion: null,
+      currentVersion,
+      latestVersion,
       message: "Major versions do not match",
     };
   }
   if (currentSemver.minor !== latestSemver.minor) {
     return {
       score: 0.5,
-      latestVersion: null,
+      currentVersion,
+      latestVersion,
       message: "Minor versions do not match",
     };
   }
   if (currentSemver.patch !== latestSemver.patch) {
     return {
       score: 0.9,
-      latestVersion: null,
+      currentVersion,
+      latestVersion,
       message: "Patch versions do not match",
     };
   }
-  return { score: 1, latestVersion, message: "Latest version is used" };
+  return {
+    score: 1,
+    currentVersion,
+    latestVersion,
+    message: "Latest version is used",
+  };
 }
 
 function getExported(graph: ModuleGraph) {
